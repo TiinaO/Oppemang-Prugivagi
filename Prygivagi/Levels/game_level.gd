1414eb70399e8d6893d0b8a700ea8732079d60ea
TiinaO <tiina.onemar@gmail.com>
2024-04-23 12:54:04 +0300

    inventory box visual fix | functionality to open and close inventory

---
+++ Prygivagi/Levels/game_level.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_inventory_gui_closed():
+	get_tree().paused = false
+
+
+func _on_inventory_gui_opened():
+	get_tree().paused = true
