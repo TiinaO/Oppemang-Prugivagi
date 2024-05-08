42125869aa1b320b7d1072304d9accfe9bc1b9f4
TiinaO <tiina.onemar@gmail.com>
2024-05-08 16:01:28 +0300

    inventory visual update | GUI update | collectable item score system implemented from old code

--- Prygivagi/Levels/game_level.gd
+++ Prygivagi/Levels/game_level.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
-func _ready():
-	pass # Replace with function body.
+@onready var invCounter: Sprite2D = $UserInterface/GameUI/InventoryInfo
 
+var score = 0 #Skoori hoidja
+
+func _ready():
+	update_score(0) # Algse skoori seadistamine
+
+func add_score(points):
+	score += points
+	update_score(score)
+
+func update_score(new_score):
+	var score_label = $UserInterface/GameUI/Score
+	score_label.text = str(new_score)
+
+#Seotud seljakoti sisu näitamisega
 func _on_inventory_gui_closed():
 	get_tree().paused = false
-
 
 func _on_inventory_gui_opened():
 	get_tree().paused = true
