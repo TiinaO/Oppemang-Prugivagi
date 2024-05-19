a018456cb3f9fe01696900b92b9b3260da088bf9
Piret <buldaspiret@gmail.com>
2024-05-19 18:40:36 +0300

    small fix

--- Prygivagi/Levels/Trash_Sorting/konteiner.gd
+++ Prygivagi/Levels/Trash_Sorting/konteiner.gd
@@ -1,25 +1,34 @@
 extends StaticBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	modulate = Color(Color.LIGHT_SLATE_GRAY, 1.0)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
-
 func _on_texture_rect_mouse_entered():
 	$TextureRect.texture = load("res://Assets/Konteinerid/Olme=Open.png")
-	$"../StaticBody2D2/TextureRect".texture = load("res://Assets/Konteinerid/Klaas=Open.png")
-	$"../StaticBody2D3/TextureRect".texture = load("res://Assets/Konteinerid/Papp-paber=Open.png")
-	$"../StaticBody2D4/TextureRect".texture = load("res://Assets/Konteinerid/Sega=Open.png")
-	$"../StaticBody2D5/TextureRect".texture = load("res://Assets/Konteinerid/Bio=Open.png")
-
 
 func _on_texture_rect_mouse_exited():
 	$TextureRect.texture = load("res://Assets/Konteinerid/Konteiner OLMEJÄÄTMED.png")
+
+func _on_klaas_mouse_entered():
+	$"../StaticBody2D2/TextureRect".texture = load("res://Assets/Konteinerid/Klaas=Open.png")
+
+func _on_klaas_mouse_exited():
 	$"../StaticBody2D2/TextureRect".texture = load("res://Assets/Konteinerid/Konteiner KLAAS.png")
+
+func _on_pappjapaber_mouse_entered():
+	$"../StaticBody2D3/TextureRect".texture = load("res://Assets/Konteinerid/Papp-paber=Open.png")
+
+func _on_pappjapaber_mouse_exited():
 	$"../StaticBody2D3/TextureRect".texture = load("res://Assets/Konteinerid/Konteiner PAPP JA PABER.png")
+
+func _on_sega_mouse_entered():
+	$"../StaticBody2D4/TextureRect".texture = load("res://Assets/Konteinerid/Sega=Open.png")
+
+func _on_sega_mouse_exited():
 	$"../StaticBody2D4/TextureRect".texture = load("res://Assets/Konteinerid/Konteiner METALL JA PLASTIK.png")
+
+func _on_bio_mouse_entered():
+	$"../StaticBody2D5/TextureRect".texture = load("res://Assets/Konteinerid/Bio=Open.png")
+
+func _on_bio_mouse_exited():
 	$"../StaticBody2D5/TextureRect".texture = load("res://Assets/Konteinerid/Konteiner BIOJÄÄTMED.png")
